3458ad5d2ff4f80dce50b67df06d943eb232696d
Agus Utomo <agusutomo@kinsta.com>
2023-06-13 14:46:37 +0700

    Added Composer File

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bd613930a8e6c3039e44b15e6818e2efbceb2b9b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bd613930a8e6c3039e44b15e6818e2efbceb2b9b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
